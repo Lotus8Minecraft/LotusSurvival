7577c97953a9e1f2b4f78a5c4859827fd7959832
Firestar311 <slaterandrew0398@gmail.com>
2024-03-18 10:40:56 -0600

    Added chestplate power

--- src/main/java/com/mclotus8esports/survival/listeners/PlayerListener.java
+++ src/main/java/com/mclotus8esports/survival/listeners/PlayerListener.java
@@ -1,16 +1,34 @@
 package com.mclotus8esports.survival.listeners;
 
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import com.mclotus8esports.survival.Items;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerListener implements Listener {
-    
+
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent e) {
         if (Items.itemIdEquals(e.getItemDrop().getItemStack(), "soul_gem")) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onArmorChange(PlayerArmorChangeEvent e) {
+        Player player = e.getPlayer();
+        if (e.getSlotType() == PlayerArmorChangeEvent.SlotType.CHEST) {
+            if (Items.itemIdEquals(e.getOldItem(), Items.wardenChestplate.getKey().getKey())) {
+                player.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+            }
+            
+            if (Items.itemIdEquals(e.getNewItem(), Items.wardenChestplate.getKey().getKey())) {
+                player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, 2, false, false));
+            }
         }
     }
 }
